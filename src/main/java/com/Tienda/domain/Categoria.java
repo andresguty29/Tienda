87c92ab6efaa490de87461ee80ba5935dc2d0411
andresguty29 <andgutierrez29@gmail.com>
2023-10-22 17:04:09 -0600

    Semana #5

---
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -0,0 +1,36 @@
+package com.Tienda.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data // para la creacion automatica de los sets y gets de los atributos
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria; // Transforma en id_categoria
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+    
+}
