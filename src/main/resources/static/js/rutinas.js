f0158cb76e3ff3a9742e6f1688d48243af2182d1
andresguty29 <andgutierrez29@gmail.com>
2023-12-05 11:27:02 -0600

    Semana # 12

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,3 +1,15 @@
+$(document).ready(function (){
+    if($("#resultsBlock").length > 0){
+    cargaLista();
+    }
+});
+
+function cargaLista(){
+    var url = '/refrescarBoton';
+    $("#resultsBlock").load(url);
+}
+
+
 /* La siguiente función se utiliza para visualizar la imagen seleccionada en la
  * página html donde se desea "cargar" utilizando un llamado "ajax"*/
 function readURL(input) {
